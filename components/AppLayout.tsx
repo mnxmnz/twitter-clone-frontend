6734aafe37d0a677a3852d5b5c57272dfabad8dd
mnxmnz <codi0703@naver.com>
2021-09-05 20:56:25 +0900

    feat: add Link tag to default menu
    상단 메뉴에 페이지 링크 생성

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 type LayoutProps = {
   children: React.ReactNode;
 };
@@ -5,7 +7,17 @@ type LayoutProps = {
 function AppLayout({ children }: LayoutProps) {
   return (
     <div>
-      <div>Menu</div>
+      <div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/profile">
+          <a>Profile</a>
+        </Link>
+        <Link href="/signup">
+          <a>Signup</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
