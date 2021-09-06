d55b9fad2e97dc0975ecbe6eb67787036e28c762
mnxmnz <codi0703@naver.com>
2021-09-07 02:51:17 +0900

    feat: antd grid view on layout
    antd Row Col 사용해서 layout 컴포넌트 grid 뷰 생성하기

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
+import UserProfile from './UserProfile';
+import LoginForm from './LoginForm';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 function AppLayout({ children }: LayoutProps) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -20,12 +25,27 @@ function AppLayout({ children }: LayoutProps) {
           </Link>
         </Menu.Item>
         <Menu.Item key="2">
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item key="3">
           <Link href="/signup">
             <a>Signup</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://github.com/mnxmnz" target="_blank" rel="noreferrer noopener">
+            Made by MinJi
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 }
