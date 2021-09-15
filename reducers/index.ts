229ca5d2d92d9748d4a2424db5bf45971f1ac332
mnxmnz <codi0703@naver.com>
2021-09-15 10:09:05 +0900

    feat: redux reducer rootstate type definition

--- reducers/index.ts
+++ reducers/index.ts
@@ -8,7 +8,7 @@ const rootReducer = combineReducers({
   index: (state = {}, action: AnyAction) => {
     switch (action.type) {
       case HYDRATE:
-        console.log('HYDRATE', action);
+        console.log('Reducers - HYDRATE', action);
         return { ...state, ...action.payload };
       default:
         return state;
@@ -19,3 +19,5 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
